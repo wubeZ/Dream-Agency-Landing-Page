75373b80d2d1c5277232a09e660c5d4ad5c6d61a
mac <wubezeleke@gmail.com>
2023-09-09 08:51:02 +0300

    fix: recent-work_responsive-fix

--- src/app/home/AboutUsSection.tsx
+++ src/app/home/AboutUsSection.tsx
@@ -5,13 +5,13 @@ import vector4 from "@/assets/images/Vector 9.svg"
 
 const AboutUsSection = () => {
   return (
-    <div className='dark:text-dark-textColor-50 lg:min-h-screen flex flex-col lg:flex-row gap-24 items-center'>
+    <div className='dark:text-dark-textColor-50 lg:min-h-screen flex flex-col lg:flex-row gap-32 lg:gap-24 items-center'>
         <div className='w-full h-96 lg:min-h-screen flex items-center justify-center pt-56 lg:pt-0'>
                 <div className='hidden lg:block border-dashed border-2 border-slate-800 mt-10 mr-auto w-[600px] h-[600px] rounded-full'></div>
                 <Image src={vector4} alt='vector 4' className='absolute z-10'/>
                 <Image src={ThreePeople} alt='three people image' className='object-contain absolute z-20' />
         </div>
-        <div className='w-full flex flex-col justify-center pt-12 lg:pt-0 lg:min-h-screen text-5xl px-8'>
+        <div className='w-full flex flex-col justify-center pt-20 md:pt-32 lg:pt-0 lg:min-h-screen text-5xl px-8'>
             <p className=' font-bold'>About Us</p>
             <p className='font-light pt-2'>Fake or <span className='relative z-10 after:absolute after:left-1 after:right-1 after:bottom-2 after:h-3 after:-z-10 after:bg-yellow-500 after:dark:bg-sky-800'>Real</span></p>
             <p className='text-base font-extralight py-5 w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus illo recusandae et, tenetur aut neque soluta expedita, dolor minima perferendis obcaecati sapiente quis ad! Modi aspernatur nesciunt voluptate id error!</p>
--- src/app/home/RecentWorkSection.tsx
+++ src/app/home/RecentWorkSection.tsx
@@ -3,13 +3,13 @@ import WorkCard from '../components/WorkCard'
 
 const RecentWorkSection = () => {
   return (
-    <div className='dark:text-dark-textColor-50 w-full min-h-screen p-12 flex flex-col gap-20 lg:gap-36 dark:bg-dark-backgroundLight bg-[#fff5f2] rounded-tl-[750px] lg:pl-28'>
-        <div className='flex flex-col lg:flex-row justify-between'>
-            <div className='lg:w-1/2 text-5xl'>
+    <div className='dark:text-dark-textColor-50 w-full min-h-screen p-12 lg:p-28 flex flex-col gap-20 lg:gap-36 dark:bg-dark-backgroundLight bg-[#fff5f2] rounded-tl-[750px] lg:pl-28'>
+        <div className='flex flex-col lg:flex-row justify-between gap-8 lg:gap-60'>
+            <div className='text-5xl w-full'>
             <p className='font-bold'>Our Recent Work</p>
             <p className='font-light pt-2 text-4xl lg:text-5xl'>By Our <span className='relative z-10 after:absolute after:left-1 after:right-1 after:bottom-2 after:h-3 after:-z-10 after:bg-yellow-500 after:dark:bg-sky-800'>Experts</span></p>
             </div>
-            <p className='lg:w-1/2 pt-12 lg:pt-0 '>
+            <p className='pt-12 lg:pt-0 '>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptate maiores vero qui eius aliquam sit nobis asperiores exercitationem, quisquam rem, doloribus labore ea sed nisi aperiam quam corporis provident?
             </p>
         </div>
